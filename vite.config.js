a39c68e7e55740864ad0d443fbe73360117fba9d
jomikehon <jomikehon@gmail.com>
2026-02-12 21:42:12 -0500

    Add base option to Vite config
    Added base option to Vite configuration.

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,11 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  base: '/', // 이 부분을 추가하거나 확인하세요
   build: {
     outDir: 'dist',
-    rollupOptions: {
-      output: {
-        manualChunks: undefined,
-      }
-    }
   }
 })
